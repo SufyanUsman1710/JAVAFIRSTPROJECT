af24c1d72a8bf3a96d18af027e62db483694cbe3
SufyanUsman1710 <backup.sufy10@gmail.com>
2022-10-31 12:51:54 +0500

    first project

---
+++ app.js
@@ -0,0 +1,68 @@
+// alert("Hello Javascript")
+// window.alert("Hello Javascript")
+
+
+
+// var a="pakistan";
+// alert(a);
+
+// var a;
+
+// var a=10;
+// var b=10;
+// var c=a+b;
+// alert(c);
+
+// var a;
+// var a=10;
+// var b="azampagal";
+// var c=b+a;
+// alert(c);
+
+// var a;
+// var a=10;
+// 	var b=20;
+// 	var c=a-b;
+// 	alert(c);
+
+// var a;
+// var a=10;
+// 	var b=20;
+// 	var c=a*b;
+// 	alert(c);
+
+// var a;
+// var a=10;
+// var b=20;
+
+// a++;
+// b--;
+
+// console.log(a);
+// console.log(b);
+
+// var a=10;
+// var b=a++;
+// console.log(b);
+// console.log(a);
+
+
+
+
+// var a=10;
+// var b=++a;
+// console.log(b);
+// console.log(a);
+
+// var a;
+// var a=10;
+// 	var b=20;
+// 	var c=(a+b)*a;
+// 	alert(c);
+
+
+var a=10;
+var b=20;
+var c= a++ - --b + a - b + b - --a - ++a + --b;
+
+console.log(c);
